7f0ec8ba5ca4597a09629abd01571c642f68a4d3
Dorian <dorianoriginaldesings@gmail.com>
2023-10-30 23:25:01 +0100

    jwt básico en node

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const port = 3000;
 
 // Rutas
 
@@ -10,4 +12,38 @@ app.use(express.json());
 
 // Uso de rutas
 
-app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
+// Clave secreta para firmar y verificar tokens
+const secretKey = 'tuclaveSecretaSuperSegura';
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Verifica las credenciales (esto es solo un ejemplo)
+  if (username === 'dorian' && password === '123456') {
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Credenciales incorrectas' });
+  }
+});
+
+// Ruta protegida que verifica el token antes de responder
+app.get('/protegido', (req, res) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token inválido' });
+    }
+
+    res.json({ message: 'Ruta protegida', usuario: decoded.username });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
